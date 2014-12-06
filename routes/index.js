06df1bd01cc4126c51976edb3540d09074f592bd
Robert Kuykendall <robert@rkuykendall.com>
2014-12-06 15:36:57 -0500

    Add images to books page.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    getBooks(res);
+    getBooks(res, "hardcover-fiction");
 });
 
 
-function getBooks(res) {
-    url = 'http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?callback=books&api-key=59a865c91407e86de482eb167653783a%3A8%3A70173232';
+function getBooks(res, name) {
+    base = "http://api.nytimes.com/svc/books/v3/lists/";
+    end = ".json?callback=books&api-key=59a865c91407e86de482eb167653783a%3A8%3A70173232";
+
+    url = base + name + end;
 
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
