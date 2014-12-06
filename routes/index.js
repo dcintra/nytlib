f0f99ea0510a106b2357192c6afc81ceb288e8c5
dcintra <daniel@lcintra.com>
2014-12-06 16:13:56 -0500

    goodreads search api integration

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var libxmljs = require("libxmljs");
 
 /* GET home page. */
 router.get('/', function(req, res) {
     getBooks(res);
+});
+
+
+router.get('/search/term=:query', function(req, res) {
+    var query = req.params.query; 
+    
+    
+    // GoodReads books search
+    findBooksFromGoodReads(query,res);
+
 });
 
 
@@ -26,6 +37,34 @@ function getBooks(res) {
         };
     });
 }
+
+function findBooksFromGoodReads(query,res) {
+
+    // query = encodeURI(query);
+    // goodreads = "https://www.goodreads.com/search/index.xml?key=ouR8rzRRu8T2kt6UrrD9w&q="+query;
+    goodreads = "https://www.goodreads.com/search.xml?key=ouR8rzRRu8T2kt6UrrD9w&q=Ender%27s+Game"
+    request(goodreads, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+
+           var xmlDoc = libxmljs.parseXml(body);
+           var gchild = xmlDoc.get('//title');
+
+            // jbody = JSON.parse(body);
+            // var books = jbody.results.books;
+            // book_title = books[0].title;
+
+            console.log(gchild.text());
+
+            // res.render('index', {
+            //     title: "Hardcover Fiction",
+            //     books: books
+            // });
+        };
+    });
+
+
+}
+
 
 
 module.exports = router;
