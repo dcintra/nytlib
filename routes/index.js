5c7fb4b53967cdafe4650efe72359777cf93dbd4
Robert Kuykendall <robert@rkuykendall.com>
2014-12-06 14:25:27 -0500

    Non-working jQ code.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-var nytEngine = require('../nyt');
-
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: nytEngine.getBooks() });
+    res.render('index', { title: getBooks() });
 });
 
 module.exports = router;
+
+
+function getBooks() {
+    url = "http://api.nytimes.com/svc/books/{version}/lists.json";
+    api_key = 'api-key=af3a02b727a9e4231ab4af07cf49970f%3A15%3A70173232';
+    url = url + '?' + api_key;
+    $.ajax({
+        'url': url,
+        'type': 'GET',
+        'dataType': "jsonp",
+        success: function(data, textStats, XMLHttpRequest) {
+            return "success";
+        },
+        error: function(data, textStatus, errorThrown) {
+            return "error";
+        }
+    });
+}
