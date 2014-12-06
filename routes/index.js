2fcd5c76fe45a9ad80686143e24e2d6b2ce51912
Robert Kuykendall <robert@rkuykendall.com>
2014-12-06 15:06:15 -0500

    Working homepage.

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,23 @@ router.get('/', function(req, res) {
 });
 
 
-
-
 function getBooks(res) {
-
     url = 'http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?callback=books&api-key=59a865c91407e86de482eb167653783a%3A8%3A70173232';
-    
+
     request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
             jbody = JSON.parse(body);
             var books = jbody.results.books;
-            book_title = books[0].title;
-            
+            // book_title = books[0].title;
+
             console.log(books);
 
-            res.render('index', { 
-                                        title: book_title
-                                     });             
+            res.render('index', {
+                title: "Hardcover Fiction",
+                books: books
+            });
         };
     });
-
-    
 }
 
 
