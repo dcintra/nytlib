e4e6245747d7f0a3d5dfe7f9d0de5c0264409c6f
dcintra <daniel@lcintra.com>
2014-12-07 14:40:10 -0500

    removed libxml

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var libxmljs = require("libxmljs");
 
 var categories = [
     "combined-print-and-e-book-nonfiction",
